0aaa0ac5cf0d910528ee34485ec9527c8f5bc5c2
John Lloyd Lawas <jlawas06@gmail.com>
2026-03-17 03:08:39 +0800

    feat: Configure project for NPM publishing with a new GitHub Actions workflow and updated build settings.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['cjs', 'esm', 'iife'],
-  globalName: 'CRXLens',
+  format: ['cjs', 'esm'],
   dts: true,
   clean: true,
   minify: true,
